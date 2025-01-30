2950c5ba6ce16def0cac685709e8a2a0b3f25b77
Tanmay <144552425+babyo77@users.noreply.github.com>
2025-01-31 05:12:46 +0530

    Update socket.ts

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -4,7 +4,8 @@ import { io } from "socket.io-client";
 export const socket = io(process.env.SOCKET_URI || "", {
   autoConnect: false,
   withCredentials: true,
-  forceNew: true,
+  // forceNew: true,
+  retries: 3,
   // reconnectionAttempts: Infinity,
   // transports: ["websocket"],
 });
